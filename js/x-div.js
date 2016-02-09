1ed6dcd3278fed39811654cbc21d9b4a7951666f
Marcelo Lazaroni <lazaronijunior@gmail.com>
2016-02-09 14:20:45 +0000

    Put everything into one file

--- js/x-div.js
+++ js/x-div.js
@@ -23,6 +23,26 @@
     this.appendChild(scriptEl);
   };
 
+  var xProto2 = Object.create(xProto);
+
   //Register the Element
   document.registerElement('x-div', { prototype: xProto });
 }());
+
+
+// ===================================
+//      Controller
+// ===================================
+
+// Calls a function and gives it an element to act on.
+function xController(func) {
+ 'use strict';
+
+ var execute = function () {
+   func(document.currentScript.parentElement);
+ };
+
+ // For document.currentScript.parentElement to give us the right element
+ // it must be called from within the function.
+ execute.call(func);
+}
